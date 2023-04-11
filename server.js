29960897764490b0633061926782951965fe2964
Sobhan Mohammadi <S.mohammadi@ArianSystemdp.local>
2023-04-11 18:06:05 +0330

    third

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const { MongoClient } = require('mongodb')
+const {isEmptyPayload , isInvalidEmail} = require('./Validator')
 
 const url = 'mongodb://127.0.0.1:27017'
 const client = new MongoClient(url)
@@ -15,35 +16,38 @@ app.use(bodyParser.json())
 app.use('/', express.static(__dirname + '/dist'))
 
 app.get('/get-profile', async function (req, res) {
-    const response = {
-        name: "sobhan",
-        email: "stizer1374@gmail.com",
-        interests: "Guitar"
-    }
-
     await client.connect()
-    console.log("connected to mongo")
+    const db = client.db(dbName)
+    const collection = db.collection(collName)
+    const results = await collection.findOne({ id: 1 })
+    client.close()
 
+    response = {}
+    if (results !== null) {
+        response = {
+            name: results.name,
+            email: results.email,
+            interests: results.interests
+        }
+    }
     res.send(response)
 })
 
 app.post('/update-profile', async function (req, res) {
     const payload = req.body
-    console.log(payload)
 
-    if (Object.keys(payload).length === 0) {
+    if (isEmptyPayload(payload) || isInvalidEmail(payload)) {
         res.status(400).send({ error: "empty payload. couldnt update" })
     } else {
         await client.connect()
-        console.log("connected to mongo")
         const db = client.db(dbName)
         const collection = db.collection(collName)
         payload['id'] = 1
-        const updatedValues = { $set: payload}
-        await collection.updateOne({id: 1}, updatedValues, {upsert: true })
+        const updatedValues = { $set: payload }
+        await collection.updateOne({ id: 1 }, updatedValues, { upsert: true })
         client.close()
         res.status(200).send({ info: "user profile updated successfully" })
-        
+
 
     }
 })
