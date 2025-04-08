10c200d7d6b05945308bf834db2a9cb5c05d412e
Kimia Hasannezhad <kimia.hasannezhad@stud-mail.uni-wuerzburg.de>
2025-04-08 10:30:19 +0200

    new begin

--- NodeJS/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use('/', express.static(__dirname + '/'))
-
-app.listen(3000, function(){
-    console.log("app listeneing on port 3000")
-})
